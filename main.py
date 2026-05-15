de623cd27594fa245a927d8d3af699a23746ae2f
Ldrsolut1on <leandrooliveiramartins01@gmail.com>
2026-05-15 15:39:21 -0300

    Refatoração: limpeza de comentários e organização do código

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from adafruit_hid.keycode import Keycode
 
 kbd = Keyboard(usb_hid.devices)
 
-# Organizando os pinos na ordem física que você definiu
 pinos_id = [board.GP0, board.GP1, board.GP4, board.GP2, board.GP3]
 botoes = []
 
@@ -20,20 +19,17 @@ for p in pinos_id:
 # Mapeamento de teclas
 atalhos = [Keycode.ONE, Keycode.TWO, Keycode.THREE, Keycode.FOUR, Keycode.FIVE]
 
-print("🚀 MacroPad Otimizado - Pronto!")
+print(" MacroPad Otimizado - Pronto!")
 
 while True:
     for i, botao in enumerate(botoes):
-        if not botao.value:  # Botão pressionado (LOW)
-            # Envia a combinação de forma mais segura
+        if not botao.value:  
             kbd.send(Keycode.CONTROL, Keycode.ALT, atalhos[i])
             
             print(f"Atalho {i+1} enviado!")
             
-            # --- MELHORIA AQUI: ESPERA SOLTAR ---
-            # Enquanto o botão estiver pressionado, ele fica preso neste loop vazio
-            # Isso evita que o comando seja enviado mil vezes se você segurar o botão
+            
             while not botao.value:
                 time.sleep(0.01) 
                 
-    time.sleep(0.01) # Small delay para poupar energia da CPU
+    time.sleep(0.01) 
